be73179bfac0679671839603c8e59a2bad725fb7
Coldnerdnacho <145206175+Coldnerdnacho@users.noreply.github.com>
2025-03-13 12:34:59 +0530

    Add files via upload

---
+++ SAPDS/script.js
@@ -0,0 +1,4 @@
+// JavaScript for menu icon (if needed for mobile)
+document.querySelector(".menu-icon").addEventListener("click", function() {
+    alert("Menu button clicked! Add functionality here.");
+});
